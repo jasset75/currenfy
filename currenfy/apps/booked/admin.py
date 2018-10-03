6db83e45dcb4abc58e4d442270d74bd3d5801ced
Juan Antonio Aguilar <juanantonioaguilar@gmail.com>
2018-10-03 22:32:39 +0200

    sell_amount field calculated

--- currenfy/apps/booked/admin.py
+++ currenfy/apps/booked/admin.py
@@ -4,7 +4,7 @@ from .models import BookedTrades
 # Booked Trades Admin
 @admin.register(BookedTrades)
 class BookedTradesAdmin(admin.ModelAdmin):
-    readonly_fields = ['ID']
+    readonly_fields = ['ID', 'buy_amount']
     list_display = ['ID', 'sell_currency', 'sell_amount', 'buy_currency', 'buy_amount', 'rate', 'date_booked']
     ordering = ['-date_booked']
     search_fields = ['ID', 'sell_currency', 'sell_amount', 'buy_currency', 'buy_amount', 'rate', 'date_booked']
