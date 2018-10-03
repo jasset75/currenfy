b3ec30bc2eda230fbeb52576d9a59d49f740b8a5
Juan A. Aguilar <juanantonioaguilar@gmail.com>
2018-10-03 09:13:22 -0700

    booked trades model start up

---
+++ currenfy/apps/booked/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import BookedTrades
+
+# Booked Trades Admin
+@admin.register(BookedTrades)
+class BookedTradesAdmin(admin.ModelAdmin):
+    readonly_fields = ['ID']
+    list_display = ['ID', 'sell_currency', 'sell_amount', 'buy_currency', 'buy_amount', 'rate', 'date_booked']
+    ordering = ['-date_booked']
+    search_fields = ['ID', 'sell_currency', 'sell_amount', 'buy_currency', 'buy_amount', 'rate', 'date_booked']
+    date_hierarchy = 'date_booked'
