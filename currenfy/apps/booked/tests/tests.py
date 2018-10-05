db64ac95fabe65069a3c2c5e115484d2805c8faf
Juan A. Aguilar <juanantonioaguilar@gmail.com>
2018-10-05 06:46:23 -0700

    improving test coverage

--- currenfy/apps/booked/tests/tests.py
+++ currenfy/apps/booked/tests/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from booked.models import BookedTrades
+from booked.models import ID_HEADER, ID_LENGTH
 
-# Create your tests here.
+
+# Basic CRUD tests
+class CrudTest(TestCase):
+
+    def check_id(self, id):
+        # checks if exists
+        self.assertTrue(id)
+        # checks if alphanumeric
+        self.assertTrue(id.isalnum())
+        # checks has the correct header
+        self.assertEqual(id[:len(ID_HEADER)], ID_HEADER)
+        # checks has the exact length
+        self.assertEqual(len(id), len(ID_HEADER)+ID_LENGTH)
+
+    def test_insert_booked_trade(self):
+        """
+            Inserts new record
+        """
+        btr = BookedTrades(
+            sell_currency='GBP',
+            sell_amount=1000,
+            buy_currency='USD',
+            rate=1.2561
+        )
+        btr.save()
+        # checks ID is ok
+        self.check_id(btr.ID)
+        # checks if trade have been correctly calculated
+        self.assertEqual(btr.buy_amount, btr.sell_amount*btr.rate)
+
+    def test_update_booked_trade(self):
+        """
+            Update record
+        """
+        # insert new record
+        btr = BookedTrades(
+            sell_currency='GBP',
+            sell_amount=900,
+            buy_currency='EUR',
+            rate=1.14
+        )
+        btr.save()
+        # checks __str__ is ID
+        self.assertEqual(str(btr), btr.ID)
+        # checks if trade have been correctly calculated
+        self.assertEqual(btr.buy_amount, btr.sell_amount*btr.rate)
+        # update
+        btr.sell_amount = 1100
+        btr.save()
+        # checks if trade have been correctly calculated
+        self.assertEqual(btr.buy_amount, btr.sell_amount*btr.rate)
