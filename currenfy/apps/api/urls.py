877109d7a4efc6a2e1a58ea3c231947c869aad25
Juan A. Aguilar <juanantonioaguilar@gmail.com>
2018-10-05 11:28:49 -0700

    api for exchange rates

--- currenfy/apps/api/urls.py
+++ currenfy/apps/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.urls import include
 from rest_framework import routers
 from .views import BookedTradesSet
+from .views import get_rate 
 
 
 router = routers.DefaultRouter()
@@ -9,4 +10,5 @@ router.register(r'booked', BookedTradesSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('rate',  get_rate)
 ]
--- currenfy/apps/api/views.py
+++ currenfy/apps/api/views.py
@@ -1,5 +1,10 @@
 from booked.models import BookedTrades
+from fixer.converter import Converter
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework import status
 from .serializers import BookedTradesSerializer
 
 
@@ -9,3 +14,21 @@ class BookedTradesSet(viewsets.ModelViewSet):
     """
     queryset = BookedTrades.objects.all().order_by('-date_booked')
     serializer_class = BookedTradesSerializer
+
+
+@api_view(["GET"])
+@renderer_classes((JSONRenderer,))
+def get_rate(sell_currency, buy_currency):
+    if not sell_currency:
+        content = {'error': 'Sell currency must be specified.'}
+        return Response(content, status.HTTP_400_BAD_REQUEST)
+    if not buy_currency:
+        content = {'error': 'Buy currency must be specified.'}
+        return Response(content, status.HTTP_400_BAD_REQUEST)
+    rate = Converter.get_rate(sell_currency=sell_currency, buy_currency=buy_currency)
+    content = {
+        'sell_currency': sell_currency,
+        'buy_currency': buy_currency,
+        'rate': rate
+    }
+    return Response(content, status.HTTP_400_BAD_REQUEST)
