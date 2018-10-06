2f11d6e07899788a2decf71eb8f1c69e6aeeea39
Juan Antonio Aguilar <juanantonioaguilar@gmail.com>
2018-10-06 15:13:56 +0200

    deleting unused files

--- currenfy/apps/api/urls.py
+++ currenfy/apps/api/urls.py
@@ -2,13 +2,17 @@ from django.urls import path
 from django.urls import include
 from rest_framework import routers
 from .views import BookedTradesSet
-from .views import get_rate 
+from .views import RateView 
 
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
+
+
+#router.register(r'rate', RateView, base_name='rate')
+
 router.register(r'booked', BookedTradesSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('rate',  get_rate)
+    path('fixer/<str:sell_currency>/<str:buy_currency>', RateView.as_view()),
 ]
--- currenfy/apps/api/views.py
+++ currenfy/apps/api/views.py
@@ -1,8 +1,9 @@
 from booked.models import BookedTrades
 from fixer.converter import Converter
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import status
 from .serializers import BookedTradesSerializer
@@ -16,19 +17,31 @@ class BookedTradesSet(viewsets.ModelViewSet):
     serializer_class = BookedTradesSerializer
 
 
-@api_view(["GET"])
-@renderer_classes((JSONRenderer,))
-def get_rate(sell_currency, buy_currency):
-    if not sell_currency:
-        content = {'error': 'Sell currency must be specified.'}
-        return Response(content, status.HTTP_400_BAD_REQUEST)
-    if not buy_currency:
-        content = {'error': 'Buy currency must be specified.'}
-        return Response(content, status.HTTP_400_BAD_REQUEST)
-    rate = Converter.get_rate(sell_currency=sell_currency, buy_currency=buy_currency)
-    content = {
-        'sell_currency': sell_currency,
-        'buy_currency': buy_currency,
-        'rate': rate
-    }
-    return Response(content, status.HTTP_400_BAD_REQUEST)
+class RateView(ListAPIView):
+    """
+        Implements fixer services interface for booking trades
+    """
+    @classmethod
+    def get_extra_actions(cls):
+        return []
+    
+    def get(self, request, sell_currency='EUR', buy_currency='GBP', format=None):
+        """
+            Return a list of all users.
+        """
+        
+        renderer_classes = (JSONRenderer, BrowsableAPIRenderer)
+
+        if not sell_currency:
+            content = {'error': 'Sell currency must be specified.'}
+            return Response(content, status.HTTP_400_BAD_REQUEST)
+        if not buy_currency:
+            content = {'error': 'Buy currency must be specified.'}
+            return Response(content, status.HTTP_400_BAD_REQUEST)
+        rate = Converter.get_rate(sell_currency=sell_currency, buy_currency=buy_currency)
+        content = {
+            'sell_currency': sell_currency,
+            'buy_currency': buy_currency,
+            'rate': rate
+        }
+        return Response(content, status.HTTP_200_OK)
