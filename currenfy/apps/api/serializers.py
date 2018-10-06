cf5bbb1b16fbf2ffae6272ce8f42706f9b65d3f8
Juan Antonio Aguilar <juanantonioaguilar@gmail.com>
2018-10-06 15:25:15 +0200

    common constants better in settigs

--- currenfy/apps/api/serializers.py
+++ currenfy/apps/api/serializers.py
@@ -1,14 +1,15 @@
 from booked.models import BookedTrades
-from booked.models import MAX_AMOUNT_LEN, ID_HEADER, ID_LENGTH
+from django.conf import settings
 from rest_framework import serializers
 
 
 class BookedTradesSerializer(serializers.Serializer):
-    ID = serializers.CharField(read_only=True, max_length=len(ID_HEADER)+ID_LENGTH)
+    
+    ID = serializers.CharField(read_only=True, max_length=len(settings.ID_HEADER) + settings.ID_LENGTH)
     sell_currency = serializers.CharField(max_length=3, required=True)
-    sell_amount = serializers.DecimalField(max_digits=MAX_AMOUNT_LEN, decimal_places=2, required=True)
+    sell_amount = serializers.DecimalField(max_digits=settings.MAX_AMOUNT_LEN, decimal_places=2, required=True)
     buy_currency = serializers.CharField(max_length=3, required=True)
-    buy_amount = serializers.DecimalField(read_only=True, max_digits=MAX_AMOUNT_LEN, decimal_places=2)
+    buy_amount = serializers.DecimalField(read_only=True, max_digits=settings.MAX_AMOUNT_LEN, decimal_places=2)
     rate = serializers.DecimalField(max_digits=9, decimal_places=4, required=True)
     date_booked = serializers.DateTimeField(read_only=True)
 
