2d255682e598cafa4da7ef4395db11a7f17d3fad
Juan Antonio Aguilar <juanantonioaguilar@gmail.com>
2018-10-06 15:22:23 +0200

    new urls for fixer api interface

--- currenfy/apps/api/urls.py
+++ currenfy/apps/api/urls.py
@@ -2,17 +2,14 @@ from django.urls import path
 from django.urls import include
 from rest_framework import routers
 from .views import BookedTradesSet
-from .views import RateView 
+from .views import RateView, SymbolsView 
 
 
 router = routers.SimpleRouter()
-
-
-#router.register(r'rate', RateView, base_name='rate')
-
-router.register(r'booked', BookedTradesSet)
+router.register(r'booked', BookedTradesSet, base_name='booked')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('fixer/<str:sell_currency>/<str:buy_currency>', RateView.as_view()),
+    path('fixer/rate/<str:sell_currency>/<str:buy_currency>', RateView.as_view(), name='fixer-rate'),
+    path('fixer/symbols', SymbolsView.as_view(), name='fixer-symbols'),
 ]
