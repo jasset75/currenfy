5dbe2d5a61a3b536f14a1a245fba77a55b6433a3
Juan Antonio Aguilar <juanantonioaguilar@gmail.com>
2018-10-08 01:17:01 +0200

    url template support for new_trade view

--- currenfy/apps/trader/urls.py
+++ currenfy/apps/trader/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import trader
+from .views import booked_trades, new_trade
 
 urlpatterns = [
-    path('', trader),
+    path('', booked_trades, name='booked_trades_list'),
+    path('new_trade', new_trade, name= 'new_trade'),
 ]
--- currenfy/apps/trader/views.py
+++ currenfy/apps/trader/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
-def trader(request):
+
+def booked_trades(request):
     return render(request, 'trader/index.html')
+
+def new_trade(request):
+    return render(request, 'trader/new_trade.html')
